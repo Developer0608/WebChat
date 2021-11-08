1325c530355b5561119361fb9dfb21e9dc49e9b7
Coder-086 <hazad9999@gmail.com>
2021-11-08 08:48:30 +0530

    Messeging were started

---
+++ target/classes/META-INF/resources/static/js/contactlist.js
@@ -0,0 +1,39 @@
+console.log('I am in contactList ');
+
+
+function getContact(email) {
+
+    console.log('I am in contact function');
+    console.log('EMAIL' , email);
+
+    const domain = getDomain();
+
+    console.log(domain);
+
+    fetch(`${domain}/contact-list/${email}`, {
+        method: "GET",
+        headers: {
+            "Content-type" : "application/json",
+            "Authorization": localStorage.getItem("token")
+        },
+    }).then(data => {
+        if(data.status == 200){
+            console.log('I am in if section')
+            return data.json();
+        }else{
+            console.log('I am in else section');
+        }
+    })
+    .then(res => {
+        if(res){
+            console.log('Displaying Response');
+            for (const obj of res) {
+                $('<li class="contact" id="' + obj.email + '" onclick=\"(getActiveMessage(\'' + obj.email  + '\'))"\><div class="wrap"><div class="meta"><p class="name">'+ obj.username +'</p></li>').appendTo($('#contacts ul'));
+            }
+            console.log(res);
+        }
+    }) 
+    .catch(err => {
+		console.log('Some error occured', err);
+	})
+}
---
+++ target/classes/META-INF/resources/static/js/message.js
@@ -0,0 +1,51 @@
+function getActiveMessage(email) {
+
+    console.log('I am in message function' , email);
+    console.log('EMAIL' , email);
+
+    const domain = getDomain();
+
+    console.log(domain);
+
+    fetch(`${domain}/messages/${email}`, {
+        method: "GET",
+        headers: {
+            "Content-type" : "application/json",
+            "Authorization": localStorage.getItem("token")
+        },
+    }).then(data => {
+        if(data.status == 200){
+            console.log('I am in if section')
+            return data.json();
+        }else{
+            console.log('I am in else section');
+        }
+    })
+    .then(res => {
+        if(res){
+            console.log('I am about to print response');
+            console.log(res);
+            
+            $('.messages ul li').remove();
+            for (const obj of res) {
+                if(obj.sender == localStorage.getItem('email')){
+                    $('<li class="sent"><p>' + obj.messages + '</p></li>').appendTo($('.messages ul'));  
+                    $('.message-input input').val(null);
+                    $('.contact.active .preview').html('<span>You: </span>' + obj.messages);
+                    $(".messages").animate({ scrollTop: $(document).height() }, "fast");
+                }
+            
+                if(obj.receiver == localStorage.getItem('email')){
+                    $('<li class="replies"><p>' + obj.messages + '</p></li>').appendTo($('.messages ul'));  
+                    $('.message-input input').val(null);
+                    $('.contact.active .preview').html('<span>You: </span>' + obj.messages);
+                    $(".messages").animate({ scrollTop: $(document).height() }, "fast");
+                }
+            }
+            
+        }
+    }) 
+    .catch(err => {
+		console.log('Some error occured', err);
+	})
+}
